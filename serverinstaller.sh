0922125aec2d14db616befb3901c843642c8359a
M4T01 <128631144+drmatoi@users.noreply.github.com>
2023-11-20 19:15:14 +0100

    Create serverinstaller.sh

---
+++ serverinstaller.sh
@@ -0,0 +1,8 @@
+atp install openjdk-17
+atp upgrade openjdk-17
+cd ~/
+mkdir minecraft && cd minecraft
+wget -O server.jar https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
+chmod +x server.jar
+echo eula=true > eula.txt
+java -Xmx1024M -Xms1024M -jar server.jar nogui
