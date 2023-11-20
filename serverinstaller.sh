f47c2d1fa6efb04266b468945f35a3a958388711
M4T01 <128631144+drmatoi@users.noreply.github.com>
2023-11-20 19:18:49 +0100

    Update serverinstaller.sh

--- serverinstaller.sh
+++ serverinstaller.sh
@@ -1,5 +1,8 @@
 atp install openjdk-17
 atp upgrade openjdk-17
+pkg install openssh
+sshd
+passwd
 cd ~/
 mkdir minecraft && cd minecraft
 wget -O server.jar https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
