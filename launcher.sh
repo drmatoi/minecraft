cb3e8ce921b660a29ec8bd04c8eec58fca8bf200
M4T01 <128631144+drmatoi@users.noreply.github.com>
2023-11-21 14:32:59 +0100

    Create launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+$Red
+# colour 
+Black="\033[1;30m"       # Black
+Red="\033[1;31m"         # Red
+Green="\033[1;32m"       # Green
+Yellow="\033[1;33m"      # Yellow
+Blue="\033[1;34m"        # Blue
+Purple="\033[1;35m"      # Purple
+Cyan="\033[1;36m"        # Cyan
+White="\033[1;37m"       # White
+clear 
+echo -e "Launcher made by M4T01 - Version: Beta 0.0.1 \033[1;31m"
+sleep 1.2
+
+clear                                                                              
+
+echo -e "
+╔══════════════════════════════════════════════════════════╦════════════════════╗
+║             github.com/drmatoi/minecraft                 ║   $Purple LAUNCHER$Cyan     ║  
+║══════════════════════════════════════════════════════════╬════════════════════╣
+║        Host a Minecraft Server using Android             ║ IG: @drmatoi       ║
+║                                                          ║ TG: @drmatoi       ║
+║                  Developed by M4T01                      ╠════════════════════╣               
+║                  drmatoi.github.io                       ║   Version B-0.1    ║
+╠══════════════════════════════════════════════════════════╬════════════════════╣
+║  $RedBugs ore problems? Write m4t01@proton.me           $White     ║     MIT LICENSE    ║
+╠══════════════════════════════════════════════════════════╩════════════════════╣
