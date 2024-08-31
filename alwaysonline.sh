e8fa94fb576a0ff243e958a6c421b58a357a2f08
M4T01 <128631144+drmatoi@users.noreply.github.com>
2024-08-31 13:34:09 +0200

    NEW STANDART RAM : 4GB

--- alwaysonline.sh
+++ alwaysonline.sh
@@ -1,5 +1,5 @@
 while true
 do
-java -Xms1G -Xmx1G -jar server.jar  // DONT FORGET TO ADJUST YOUR RAM SETTINGS IN THIS FILE! - m4t01
+java -Xms4G -Xmx4G -jar server.jar  // DONT FORGET TO ADJUST YOUR RAM SETTINGS IN THIS FILE! - m4t01
 sleep 1 
 done
